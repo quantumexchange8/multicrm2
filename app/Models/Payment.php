9ed6a992056030c5ae5d270813b9f2b43effe672
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-07-05 16:59:35 +0800

    feat: functions in models

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payment extends Model
 {
-    use HasFactory;
-}
+    use HasFactory, SoftDeletes;
+    protected $guarded = [];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'real_amount' => 'decimal:2',
+        'delete' => 'boolean',
+    ];
+
+    public function ofUser()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function toAccount()
+    {
+        return $this->belongsTo(TradingAccount::class, 'to', 'meta_login');
+    }
+    public function fromAccount()
+    {
+        return $this->belongsTo(TradingAccount::class, 'from', 'meta_login');
+    }}
--- app/Models/TradingUser.php
+++ app/Models/TradingUser.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TradingUser extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'balance' => 'decimal:2',
+        'credit' => 'decimal:2',
+        'interest_rate' => 'decimal:2',
+        'commission_daily' => 'decimal:2',
+        'commission_montly' => 'decimal:2',
+        'balance_prev_day' => 'decimal:2',
+        'balance_prev_month' => 'decimal:2',
+        'equity_prev_day' => 'decimal:2',
+        'equity_prev_month' => 'decimal:2',
+        'created_at' => 'datetime:Y-m-d',
+    ];
+
+    public function ofUser()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
