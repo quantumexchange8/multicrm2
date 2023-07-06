5fb93dea24328c1a3a009e6dec0577a07de3567e
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-07-06 14:50:52 +0800

    fix: redirect api url error, edit on master layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @if (App::environment('production'))
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        @endif
 
         <title inertia>{{ config('app.name', 'Quantum Capital Global') }}</title>
 
