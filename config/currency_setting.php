89afaec5bde757eb92f798d9e5d4254a52f18d5b
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-07-18 19:11:32 +0800

    feat: crypto deposit

--- config/currency_setting.php
+++ config/currency_setting.php
@@ -15,4 +15,11 @@ return [
         'secretKey' => '645B45FF789C431889BFFBF1E462DCE6',
         'base_url' => 'https://api.doitwallet.asia/',
     ],
+    'USDT' => [
+        'type' => 'USDT',
+        'merchantID' => '60-00000133-61973258',
+        'apiKey' => '1FAA534F-8579-4F29-9BFB-0A016ABBE67C',
+        'secretKey' => '7075226CB650486D811729E6D9359C7C',
+        'base_url' => 'https://api.doitwallet.asia/',
+    ],
 ];
