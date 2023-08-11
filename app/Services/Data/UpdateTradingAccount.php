0f47e9f71810e3b626711ab8c66ec0415f595e39
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-08-11 19:19:53 +0800

    fix: trading account update account type get id

--- app/Services/Data/UpdateTradingAccount.php
+++ app/Services/Data/UpdateTradingAccount.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Data;
 
+use App\Models\AccountType;
 use App\Models\TradingAccount;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
@@ -16,12 +17,15 @@ class UpdateTradingAccount
     public function updateTradingAccount($meta_login, $data): TradingAccount
     {
         $tradingAccount = TradingAccount::query()->where('meta_login', $meta_login)->first();
+        $tradingUser = $tradingAccount->tradingUser;
+        $accountType = AccountType::where('name', $tradingUser->meta_group)->first();
+
         $tradingAccount->currency_digits = $data['moneyDigits'];
         $tradingAccount->balance = $data['balance'] / 100;
         $tradingAccount->credit = $data['nonWithdrawableBonus'] / 100;
         $tradingAccount->margin_leverage = $data['leverageInCents'] / 100;
         $tradingAccount->equity = $data['equity'] / 100;
-        $tradingAccount->account_type = $tradingAccount->accountType->id;
+        $tradingAccount->account_type = $accountType->id;
         DB::transaction(function () use ($tradingAccount) {
             $tradingAccount->save();
         });
